61157eccbe0c712c93b6d118a2a366fe1161bc28
Caryn Keffer <carynkeffer@gmail.com>
2021-02-06 12:58:34 -0500

    continue adding feature tests for user story 4

--- spec/features/shops/edit_spec.rb
+++ spec/features/shops/edit_spec.rb
@@ -2,13 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Shops Edit,' do
   describe 'as a visitor' do
-    describe 'when I visit the shops id page' do
-      it 'can edit a shop with a form' do
-        @spicy = Shop.create!(name: 'Spicy Pickle', liquor_license: true, revenue: nil)
+    before :each do
+      @spicy = Shop.create!(name: 'Spicy Pickle', liquor_license: true, revenue: nil)
+    end
 
+    describe 'when I visit the shops id page' do
+      it 'autofills shop info' do
         visit "/shops/#{@spicy.id}/edit"
 
         expect(find_field('name').value).to eq("Spicy Pickle")
+        expect(find_field('revenue').value).to eq(nil)
+        expect(page).to have_field('license', checked: true)
+      end
+
+      it 'can edit form' do
+        visit "/shops/#{@spicy.id}/edit"
+
+        fill_in 'name', with: "Spicy"
+        fill_in 'revenue', with: "$40000"
+
+        uncheck 'license'
       end
     end
   end
