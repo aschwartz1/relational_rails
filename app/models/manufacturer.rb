69aa23de2a9003876bfaceb5af9a042a97f83223
Alex Schwartz <aschwartz@detaso.com>
2021-02-09 09:26:20 -0600

    Add query to manufacturer model

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -2,4 +2,8 @@ class Manufacturer < ApplicationRecord
   has_many :discs, dependent: :destroy
 
   validates_presence_of :name
+
+  def discs_ordered_by_creation
+    discs.order(:created_at)
+  end
 end
--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -8,4 +8,17 @@ describe Manufacturer, type: :model do
   describe 'relationships' do
     it { should have_many :discs }
   end
+
+  describe 'class methods' do
+    it '.discs_ordered_by_creation' do
+      axiom = Manufacturer.create!(name: 'Axiom', founded_in: 2014, in_business: true)
+      axiom.discs.create!(name: 'Envy', max_weight: 174, in_production: true)
+      axiom.discs.create!(name: 'Proxy', max_weight: 174, in_production: true)
+
+      expected = axiom.discs.order(:created_at)
+      actual = axiom.discs_ordered_by_creation
+
+      expect(actual).to eq expected
+    end
+  end
 end
