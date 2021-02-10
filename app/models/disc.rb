58acc19f3fef23b5433c0620c3dc84717c2e2681
Alex Schwartz <aschwartz@detaso.com>
2021-02-09 20:48:58 -0600

    Update disc with in production method

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,8 +1,7 @@
 class Disc < ApplicationRecord
   belongs_to :manufacturer
 
-  def self.in_production_by_manufacturer(manufacturer_id)
-    # where("manufacturer_id = #{manufacturer_id} AND in_production = true")
-    where("manufacturer_id = :manufacturer_id AND in_production = true", {manufacturer_id: manufacturer_id})
+  def self.in_production
+    where("in_production = true")
   end
 end
--- spec/models/disc_spec.rb
+++ spec/models/disc_spec.rb
@@ -8,11 +8,14 @@ describe Disc, type: :model do
   describe 'class methods' do
     it '.in_production_by_manufacturer' do
       axiom = Manufacturer.create!(name: 'Axiom', founded_in: 2014, in_business: true)
+      innova = Manufacturer.create!(name: 'Innova', founded_in: 1983, in_business: true)
       envy = axiom.discs.create!(name: 'Envy', max_weight: 174, in_production: true)
       proxy = axiom.discs.create!(name: 'Proxy', max_weight: 174, in_production: true)
       alias_ = axiom.discs.create!(name: 'Alias', max_weight: 178, in_production: false)
+      teebird = innova.discs.create!(name: 'Teebird', max_weight: 175, in_production: true)
+      moray = innova.discs.create!(name: 'Moray', max_weight: 179, in_production: false)
 
-      expect(Disc.in_production_by_manufacturer(axiom.id)).to eq([envy, proxy])
+      expect(Disc.in_production).to eq([envy, proxy, teebird])
     end
   end
 end
