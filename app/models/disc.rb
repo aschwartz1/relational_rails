a666f79df4ea36648c1853e6598a1d47256de022
Alex Schwartz <aschwartz@detaso.com>
2021-02-09 20:23:53 -0600

    Add filter query to discs model

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,3 +1,8 @@
 class Disc < ApplicationRecord
   belongs_to :manufacturer
+
+  def self.in_production_by_manufacturer(manufacturer_id)
+    # where("manufacturer_id = #{manufacturer_id} AND in_production = true")
+    where("manufacturer_id = :manufacturer_id AND in_production = true", {manufacturer_id: manufacturer_id})
+  end
 end
--- spec/models/disc_spec.rb
+++ spec/models/disc_spec.rb
@@ -4,4 +4,15 @@ describe Disc, type: :model do
   describe 'relationships' do
     it { should belong_to :manufacturer }
   end
+
+  describe 'class methods' do
+    it '.in_production_by_manufacturer' do
+      axiom = Manufacturer.create!(name: 'Axiom', founded_in: 2014, in_business: true)
+      envy = axiom.discs.create!(name: 'Envy', max_weight: 174, in_production: true)
+      proxy = axiom.discs.create!(name: 'Proxy', max_weight: 174, in_production: true)
+      alias_ = axiom.discs.create!(name: 'Alias', max_weight: 178, in_production: false)
+
+      expect(Disc.in_production_by_manufacturer(axiom.id)).to eq([envy, proxy])
+    end
+  end
 end
